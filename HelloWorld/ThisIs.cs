27226516e0dd63a91b926ea92fbcdea87b3feb84
Łukasz Wielopolski <lukasz.wielopolski@softwaremind.com>
2017-05-19 14:06:14 +0200

    Modyfikuje HelloWorld pod paczkę

---
+++ HelloWorld/ThisIs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class ThisIs
+    {
+        static void Main()
+        {
+            PrintMessage();
+        }
+
+        public static void PrintMessage()
+        {
+            var msg = "Hello world!";
+
+            Console.WriteLine($"Show message: {msg}");
+
+            Thread.Sleep(5000);
+        }
+    }
+}
