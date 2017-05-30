711ef9568ed100eb3b7635c7c73d8fbc020e5f91
Łukasz Wielopolski <lukasz.wielopolski@softwaremind.com>
2017-05-30 13:19:59 +0200

    gutil.log

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,14 +1,3 @@
 ﻿var gulp = require('gulp'),
-    path = require('path'),
-    folders = require('gulp-folders'),
-    pathToFolder = 'path/to/folder';
- 
-gulp.task('task', folders(pathToFolder, function(folder){
-    //This will loop over all folders inside pathToFolder main, secondary 
-    //Return stream so gulp-folders can concatenate all of them 
-    //so you still can use safely use gulp multitasking 
- 
-    return gulp.src(path.join(pathToFolder, folder, '*.js'))
-        .pipe(concat(folder + '.js'))
-        .pipe(gulp.dest('dist'));
-}));
+var gutil = require('gulp-util');
+gutil.log('Hello world!');
