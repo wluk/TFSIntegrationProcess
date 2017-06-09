d0bf0195a79708bee30675950cc4c2801e831300
Łukasz Wielopolski <lukasz.wielopolski@softwaremind.com>
2017-06-09 13:31:43 +0200

    pobrane 6

--- transform/Transform.ps1
+++ transform/Transform.ps1
@@ -9,7 +9,7 @@
 function SetTransformFiles($configTypes, $path) {
     foreach ($configType in $configTypes) {
         if (Test-Path -Path $path) {
-            $filesConfig = Get-ChildItem -filter "$configType.*.config" -File
+            $filesConfig = Get-ChildItem -path $path -filter "$configType.*.config" -File
 
             Write-Host "In path $path found: $filesConfig"
 
@@ -25,7 +25,7 @@ function SetTransformFiles($configTypes, $path) {
 
 function TransformConfigFiles($configPath, $transformPath) {
     try {
-        Write-Host "Start transform $transformPath to $configPath"
+        Write-Host "Start transformation $transformPath to $configPath"
         $XmlTransformPath = $PSScriptRoot + "\Microsoft.Web.XmlTransform.dll"
         Add-Type -LiteralPath $XmlTransformPath
         
@@ -53,18 +53,21 @@ Write-Host "Transforms source_binaries with configuration related to target envi
 
 #Web
 $webConfigsType = @("AppSettings", "log4net", "Web")
-$sourceDir = $env:Build_SourcesDirectory + "\APP\WEB"
+$sourceDir = "C:\Users\luwe\Documents\Visual Studio 2015\Projects\WebAppCI\APP\WEB"
+#$sourceDir = $env:Build_SourcesDirectory + "\APP\WEB"
 Write-Host "Start transform WEB, fransform config: $webConfigsType on path: $sourceDir"
 SetTransformFiles $webConfigsType $sourceDir
 
 #Server
 $serverConfigsType = @("AppSettings", "log4net", "Yellow.Server.exe", "quartz-job")
-$sourceDir = $env:Build_SourcesDirectory + "\APP\SERVER"
+$sourceDir = "C:\Users\luwe\Documents\Visual Studio 2015\Projects\WebAppCI\APP\SERVER"
+#$sourceDir = $env:Build_SourcesDirectory + "\APP\SERVER"
 Write-Host "Start transform SERVER, fransform config: $serverConfigsType on path: $sourceDir"
 SetTransformFiles $serverConfigsType $sourceDir
 
 #TestClient
 $testClientsConfigType = @("Web")
-$sourceDir = $env:Build_SourcesDirectory + ".\TEST\CLIENT"
+$sourceDir = "C:\Users\luwe\Documents\Visual Studio 2015\Projects\WebAppCI\TEST\CLIENT"
+#$sourceDir = $env:Build_SourcesDirectory + "\TEST\CLIENT"
 Write-Host "Start transform TestClient, fransform config: $testClientsConfigType on path: $sourceDir"
 SetTransformFiles $testClientsConfigType $sourceDir
